0299a3f5ef6a2f3ee17ba554bcfb919bb795139d
mecha2k <mecha2k@naver.com>
2022-01-03 15:16:08 +0900

    market fundamental

--- src/02 market_and_fundamental_data/02_algoseek_intraday/algoseek_minute_data.py
+++ src/02 market_and_fundamental_data/02_algoseek_intraday/algoseek_minute_data.py
@@ -1,54 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Processing Algoseek's Trade & Quote Minute Bar data
-
-# In this notebook, we load the high-quality NASDAQ100 minute-bar trade-and-quote data generously provided by [Algoseek](https://www.algoseek.com/) (available [here](https://www.algoseek.com/ml4t-book-data.html)) that we will use in [Chapter 12](../../12_gradient_boosting_machines) to develop an intraday trading strategy. 
-
-# ## Imports & Settings
-
-# In[1]:
-
-
-import warnings
-
-warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
+# Processing Algoseek's Trade & Quote Minute Bar data
+# In this notebook, we load the high-quality NASDAQ100 minute-bar trade-and-quote data generously provided by
+# [Algoseek](https://www.algoseek.com/) (available [here](https://www.algoseek.com/ml4t-book-data.html)) that we will
+# use in [Chapter 12](../../12_gradient_boosting_machines) to develop an intraday trading strategy.
 
 from pathlib import Path
 from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
 
-
-# In[3]:
-
-
-sns.set_style('whitegrid')
 idx = pd.IndexSlice
+sns.set_style("whitegrid")
+sns.set_palette("pastel")
+plt.rcParams["figure.dpi"] = 300
+plt.rcParams["font.size"] = 16
+warnings.filterwarnings("ignore")
 
-
-# ## Algoseek Trade & Quote Minute Bar Data
-
-# ### Data Dictionary
-
-# The Quote fields are based on changes to the NBBO ([National Best Bid Offer](https://www.investopedia.com/terms/n/nbbo.asp)) from the top-of-book price and size from
+## Algoseek Trade & Quote Minute Bar Data
+### Data Dictionary
+# The Quote fields are based on changes to the NBBO ([National Best Bid Offer]
+# (https://www.investopedia.com/terms/n/nbbo.asp)) from the top-of-book price and size from
 # each of the exchanges.
-# 
+#
 # The enhanced Trade & Quote bar fields include the following fields:
 # - **Field**: Name of Field.
 # - **Q / T**: Field based on Quotes or Trades
 # - **Type**: Field format
-# - **No Value**: Value of field when there is no value or data. 
+# - **No Value**: Value of field when there is no value or data.
 #   - Note: “Never” means field should always have a value EXCEPT for the first bar of the day.
 # - **Description**: Description of the field.
 
@@ -109,236 +90,183 @@ idx = pd.IndexSlice
 # | 53  | `RepeatDowntickVolume`  | T    | Integer                       | 0         | Total number of shares where trade price is the same (repeated) and last price change was down during bar. <br/>Repeat downtick = ( trade price == last trade price ) & (last tick direction == down )                                                                                                                                   |
 # | 54  | `UnknownVolume`         | T    | Integer                       | 0         | When the first trade of the day takes place, the tick direction is “unknown” as there is no previous Trade to compare it to.<br/>This field is the volume of the first trade after 4am and acts as an initiation value for the tick volume directions.<br/>In future this bar will be renamed to `UnkownTickDirectionVolume` .  |
 
-# ### Notes
-# 
 # **Empty Fields**
-# 
-# An empty field has no value and is “Blank” , for example FirstTradeTime and there are no trades during the bar period. 
-# The field `Volume` measuring total number of shares traded in bar will be `0` if there are no Trades (see `No Value` column above for each field).
-# 
+# An empty field has no value and is “Blank” , for example FirstTradeTime and there are no trades during the bar period.
+# The field `Volume` measuring total number of shares traded in bar will be `0` if there are no Trades (see `No Value`
+# column above for each field).
 # **No Bid/Ask/Trade OHLC**
-# 
-# During a bar timeframe there may not be a change in the NBBO or an actual Trade. 
+# During a bar timeframe there may not be a change in the NBBO or an actual Trade.
 # For example, there can be a bar with OHLC Bid/Ask but no Trade OHLC.
-# 
 # **Single Event**
-# 
 # For bars with only one trade, one NBBO bid or one NBBO ask then Open/High/Low/Close price,size andtime will be the same.
-# 
-# **`AskPriceAtHighBidPrice`, `AskSizeAtHighBidPrice`, `AskPriceAtLowBidPrice`, `AskSizeAtLowBidPrice` Fields** 
-# 
-# To provide consistent Bid/Ask prices at a point in time while showing the low/high Bid/Ask for the bar, AlgoSeek uses the low/high `Bid` and the corresponding `Ask` at that price.
-# 
+# **`AskPriceAtHighBidPrice`, `AskSizeAtHighBidPrice`, `AskPriceAtLowBidPrice`, `AskSizeAtLowBidPrice` Fields**
+# To provide consistent Bid/Ask prices at a point in time while showing the low/high Bid/Ask for the bar, AlgoSeek uses
+# the low/high `Bid` and the corresponding `Ask` at that price.
+#
 # ### FAQ
-# 
 # **Why are Trade Prices often inside the Bid Price to Ask Price range?**
-# 
-# The Low/High Bid/Ask is the low and high NBBO price for the bar range. 
-# Very often a Trade may not occur at these prices as the price may only last a few seconds or executions are being crossed at mid-point due to hidden order types that execute at mid-point or as price improvement over current `Bid`/`Ask`.
-# 
-# **How to get exchange tradable shares?** 
-# 
-# To get the exchange tradable volume in a bar subtract `Volume` from `FinraVolume`. 
-# - `Volume` is the total number of shares traded. 
-# - ``FinraVolume`` is the total number of shares traded that are reported as executions by FINRA. 
-# 
-# When a trade is done that is off the listed exchanges, it must be reported to FINRA by the brokerage firm or dark pool. Examples include: 
+# The Low/High Bid/Ask is the low and high NBBO price for the bar range.
+# Very often a Trade may not occur at these prices as the price may only last a few seconds or executions are being
+# crossed at mid-point due to hidden order types that execute at mid-point or as price improvement over
+# current `Bid`/`Ask`.
+#
+# **How to get exchange tradable shares?**
+# To get the exchange tradable volume in a bar subtract `Volume` from `FinraVolume`.
+# - `Volume` is the total number of shares traded.
+# - ``FinraVolume`` is the total number of shares traded that are reported as executions by FINRA.
+#
+# When a trade is done that is off the listed exchanges, it must be reported to FINRA by the brokerage firm or dark
+# pool. Examples include:
 # - internal crosses by broker dealer
 # - over-the-counter block trades, and
 # - dark pool executions.
+# We use the 'Trade and Quote' dataset - see [documentation]
+# (https://us-equity-market-data-docs.s3.amazonaws.com/algoseek.US.Equity.TAQ.Minute.Bars.pdf) for details on the
+# definition of the numerous fields.
 
-# ## Data prep
+tcols = [
+    "openbartime",
+    "firsttradetime",
+    "highbidtime",
+    "highasktime",
+    "hightradetime",
+    "lowbidtime",
+    "lowasktime",
+    "lowtradetime",
+    "closebartime",
+    "lasttradetime",
+]
 
-# We use the 'Trade and Quote' dataset - see [documentation](https://us-equity-market-data-docs.s3.amazonaws.com/algoseek.US.Equity.TAQ.Minute.Bars.pdf) for details on the definition of the numerous fields.
+drop_cols = ["unknowntickvolume", "cancelsize", "tradeatcrossorlocked"]
 
-# In[5]:
-
-
-tcols = ['openbartime',
-         'firsttradetime',
-         'highbidtime',
-         'highasktime',
-         'hightradetime',
-         'lowbidtime',
-         'lowasktime',
-         'lowtradetime',
-         'closebartime',
-         'lasttradetime']
-
-
-# In[6]:
-
-
-drop_cols = ['unknowntickvolume',
-             'cancelsize',
-             'tradeatcrossorlocked']
-
-
-# In[7]:
-
-
-keep = ['firsttradeprice',
-        'hightradeprice',
-        'lowtradeprice',
-        'lasttradeprice',
-        'minspread',
-        'maxspread',
-        'volumeweightprice',
-        'nbboquotecount',
-        'tradeatbid',
-        'tradeatbidmid',
-        'tradeatmid',
-        'tradeatmidask',
-        'tradeatask',
-        'volume',
-        'totaltrades',
-        'finravolume',
-        'finravolumeweightprice',
-        'uptickvolume',
-        'downtickvolume',
-        'repeatuptickvolume',
-        'repeatdowntickvolume',
-        'tradetomidvolweight',
-        'tradetomidvolweightrelative']
-
+keep = [
+    "firsttradeprice",
+    "hightradeprice",
+    "lowtradeprice",
+    "lasttradeprice",
+    "minspread",
+    "maxspread",
+    "volumeweightprice",
+    "nbboquotecount",
+    "tradeatbid",
+    "tradeatbidmid",
+    "tradeatmid",
+    "tradeatmidask",
+    "tradeatask",
+    "volume",
+    "totaltrades",
+    "finravolume",
+    "finravolumeweightprice",
+    "uptickvolume",
+    "downtickvolume",
+    "repeatuptickvolume",
+    "repeatdowntickvolume",
+    "tradetomidvolweight",
+    "tradetomidvolweightrelative",
+]
 
 # We will shorten most of the field names to reduce typing:
+columns = {
+    "volumeweightprice": "price",
+    "finravolume": "fvolume",
+    "finravolumeweightprice": "fprice",
+    "uptickvolume": "up",
+    "downtickvolume": "down",
+    "repeatuptickvolume": "rup",
+    "repeatdowntickvolume": "rdown",
+    "firsttradeprice": "first",
+    "hightradeprice": "high",
+    "lowtradeprice": "low",
+    "lasttradeprice": "last",
+    "nbboquotecount": "nbbo",
+    "totaltrades": "ntrades",
+    "openbidprice": "obprice",
+    "openbidsize": "obsize",
+    "openaskprice": "oaprice",
+    "openasksize": "oasize",
+    "highbidprice": "hbprice",
+    "highbidsize": "hbsize",
+    "highaskprice": "haprice",
+    "highasksize": "hasize",
+    "lowbidprice": "lbprice",
+    "lowbidsize": "lbsize",
+    "lowaskprice": "laprice",
+    "lowasksize": "lasize",
+    "closebidprice": "cbprice",
+    "closebidsize": "cbsize",
+    "closeaskprice": "caprice",
+    "closeasksize": "casize",
+    "firsttradesize": "firstsize",
+    "hightradesize": "highsize",
+    "lowtradesize": "lowsize",
+    "lasttradesize": "lastsize",
+    "tradetomidvolweight": "volweight",
+    "tradetomidvolweightrelative": "volweightrel",
+}
 
-# In[8]:
+# The Algoseek minute-bar data comes in compressed csv files that contain the data for one symbol and day, organized in
+# three directories for each year (2015-17). The function `extract_and_combine_data` reads the ~80K source files and
+# combines them into a single `hdf5` file for faster access.
+# >The data is fairly large (>8GB), and if you run into memory constraints, please modify the code to process the data
+# in smaller chunks. One options is to iterate over the three directories containing data for a single year only, and
+# storing each year separately.
 
-
-columns = {'volumeweightprice': 'price',
-           'finravolume': 'fvolume',
-           'finravolumeweightprice': 'fprice',
-           'uptickvolume': 'up',
-           'downtickvolume': 'down',
-           'repeatuptickvolume': 'rup',
-           'repeatdowntickvolume': 'rdown',
-           'firsttradeprice': 'first',
-           'hightradeprice': 'high',
-           'lowtradeprice': 'low',
-           'lasttradeprice': 'last',
-           'nbboquotecount': 'nbbo',
-           'totaltrades': 'ntrades',
-           'openbidprice': 'obprice',
-           'openbidsize': 'obsize',
-           'openaskprice': 'oaprice',
-           'openasksize': 'oasize',
-           'highbidprice': 'hbprice',
-           'highbidsize': 'hbsize',
-           'highaskprice': 'haprice',
-           'highasksize': 'hasize',
-           'lowbidprice': 'lbprice',
-           'lowbidsize': 'lbsize',
-           'lowaskprice': 'laprice',
-           'lowasksize': 'lasize',
-           'closebidprice': 'cbprice',
-           'closebidsize': 'cbsize',
-           'closeaskprice': 'caprice',
-           'closeasksize': 'casize',
-           'firsttradesize': 'firstsize',
-           'hightradesize': 'highsize',
-           'lowtradesize': 'lowsize',
-           'lasttradesize': 'lastsize',
-           'tradetomidvolweight': 'volweight',
-           'tradetomidvolweightrelative': 'volweightrel'}
-
-
-# The Algoseek minute-bar data comes in compressed csv files that contain the data for one symbol and day, organized in three directories for each year (2015-17). The function `extract_and_combine_data` reads the ~80K source files and combines them into a single `hdf5` file for faster access. 
-# 
-# >The data is fairly large (>8GB), and if you run into memory constraints, please modify the code to process the data in smaller chunks. One options is to iterate over the three directories containing data for a single year only, and storing each year separately.
-
-# In[4]:
-
-
-nasdaq_path = Path('../../data/nasdaq100')
-
-
-# In[14]:
+nasdaq_path = Path("../../data/nasdaq100")
 
 
 def extract_and_combine_data():
-    path = nasdaq_path / '1min_taq'
+    path = nasdaq_path / "1min_taq"
     if not path.exists():
         path.mkdir(parents=True)
 
     data = []
     # ~80K files to process
-    for f in tqdm(list(path.glob('*/**/*.csv.gz'))):
-        data.append(pd.read_csv(f, parse_dates=[['Date', 'TimeBarStart']])
-                    .rename(columns=str.lower)
-                    .drop(tcols + drop_cols, axis=1)
-                    .rename(columns=columns)
-                    .set_index('date_timebarstart')
-                    .sort_index()
-                    .between_time('9:30', '16:00')
-                    .set_index('ticker', append=True)
-                    .swaplevel()
-                    .rename(columns=lambda x: x.replace('tradeat', 'at')))
-    data = pd.concat(data).apply(pd.to_numeric, downcast='integer')
-    data.index.rename(['ticker', 'date_time'], inplace=True)
+    for f in tqdm(list(path.glob("*/**/*.csv.gz"))):
+        data.append(
+            pd.read_csv(f, parse_dates=[["Date", "TimeBarStart"]])
+            .rename(columns=str.lower)
+            .drop(tcols + drop_cols, axis=1)
+            .rename(columns=columns)
+            .set_index("date_timebarstart")
+            .sort_index()
+            .between_time("9:30", "16:00")
+            .set_index("ticker", append=True)
+            .swaplevel()
+            .rename(columns=lambda x: x.replace("tradeat", "at"))
+        )
+    data = pd.concat(data).apply(pd.to_numeric, downcast="integer")
+    data.index.rename(["ticker", "date_time"], inplace=True)
     print(data.info(show_counts=True))
-    data.to_hdf(nasdaq_path / 'algoseek.h5', 'min_taq')
-
-
-# In[15]:
+    data.to_hdf(nasdaq_path / "algoseek.h5", "min_taq")
 
 
 extract_and_combine_data()
 
+df = pd.read_hdf(nasdaq_path / "algoseek.h5", "min_taq")
+df.info(show_counts=True)
 
-# In[9]:
-
-
-df = pd.read_hdf(nasdaq_path / 'algoseek.h5', 'min_taq')
-
-
-# In[10]:
-
-
-df.info(null_counts=True)
-
-
-# ## NASDAQ 100 Constituents
-
+## NASDAQ 100 Constituents
 # The dataset contains 142 stocks because there were multiple changes to index membership over the 2015-17 period:
+print(len(df.index.unique("ticker")))
 
-# In[11]:
-
-
-len(df.index.unique('ticker'))
-
-
-# The below heatmap highlights the frequent entry/exit points of various securities, which emphasizes the need for a survivorship-free dataset.
-
-# In[53]:
-
-
-constituents = (df.groupby([df.index.get_level_values('date_time').date, 'ticker'])
-                .size()
-                .unstack('ticker')
-                .notnull()
-                .astype(int)
-                .replace(0, np.nan))
-
+# The below heatmap highlights the frequent entry/exit points of various securities, which emphasizes the need for
+# a survivorship-free dataset.
+constituents = (
+    df.groupby([df.index.get_level_values("date_time").date, "ticker"])
+    .size()
+    .unstack("ticker")
+    .notnull()
+    .astype(int)
+    .replace(0, np.nan)
+)
 constituents.index = pd.to_datetime(constituents.index)
-constituents = constituents.resample('M').max()
+constituents = constituents.resample("M").max()
 constituents.index = constituents.index.date
-
-
-# In[63]:
-
 
 fig, ax = plt.subplots(figsize=(20, 20))
 mask = constituents.T.isnull()
-ax = sns.heatmap(constituents.T, mask=mask, cbar=False, ax=ax, cmap='Blues_r')
-ax.set_ylabel('')
-fig.suptitle('NASDAQ100 Constituents (2015-2017)')
-fig.tight_layout();
-
-
-# In[ ]:
-
-
-
-
+ax = sns.heatmap(constituents.T, mask=mask, cbar=False, ax=ax, cmap="Blues_r")
+ax.set_ylabel("")
+fig.suptitle("NASDAQ100 Constituents (2015-2017)")
+fig.tight_layout()
+plt.savefig("images/01-01.png", bboxinches="tight")
--- src/02 market_and_fundamental_data/03_data_providers/02_yfinance_demo.py
+++ src/02 market_and_fundamental_data/03_data_providers/02_yfinance_demo.py
@@ -1,216 +1,70 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Downloading Market and Fundamental Data with `yfinance`
-
-# ## Imports & Settings
-
-# In[1]:
-
-
-import warnings
-warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
-
 import pandas as pd
 import yfinance as yf
+from pandas_datareader import data as pdr
+import warnings
 
+warnings.filterwarnings("ignore")
 
-# ## How to work with a Ticker object
-
-# In[3]:
-
-
-symbol = 'FB'
+symbol = "FB"
 ticker = yf.Ticker(symbol)
+print(pd.Series(ticker.info).head(20))
 
-
-# ### Show ticker info
-
-# In[4]:
-
-
-pd.Series(ticker.info).head(20)
-
-
-# ### Get market data
-
-# In[5]:
-
-
-data = ticker.history(period='5d',
-                      interval='1m',
-                      start=None,
-                      end=None,
-                      actions=True,
-                      auto_adjust=True,
-                      back_adjust=False)
+data = ticker.history(
+    period="5d",
+    interval="1m",
+    start=None,
+    end=None,
+    actions=True,
+    auto_adjust=True,
+    back_adjust=False,
+)
 data.info()
 
-
-# ### View company actions
-
-# In[6]:
-
-
+### View company actions
 # show actions (dividends, splits)
-ticker.actions
+print(ticker.actions)
+print(ticker.dividends)
+print(ticker.splits)
 
+### Annual and Quarterly Financial Statement Summary
+print(ticker.financials)
+print(ticker.quarterly_financials)
 
-# In[7]:
+### Annual and Quarterly Balance Sheet
+print(ticker.balance_sheet)
+print(ticker.quarterly_balance_sheet)
 
+### Annual and Quarterly Cashflow Statement
+print(ticker.cashflow)
+print(ticker.quarterly_cashflow)
+print(ticker.earnings)
+print(ticker.quarterly_earnings)
 
-ticker.dividends
+### Sustainability: Environmental, Social and Governance (ESG)
+print(ticker.sustainability)
 
-
-# In[8]:
-
-
-ticker.splits
-
-
-# ### Annual and Quarterly Financial Statement Summary
-
-# In[9]:
-
-
-ticker.financials
-
-
-# In[10]:
-
-
-ticker.quarterly_financials
-
-
-# ### Annual and Quarterly Balance Sheet
-
-# In[11]:
-
-
-ticker.balance_sheet
-
-
-# In[12]:
-
-
-ticker.quarterly_balance_sheet
-
-
-# ### Annual and Quarterly Cashflow Statement
-
-# In[13]:
-
-
-ticker.cashflow
-
-
-# In[14]:
-
-
-ticker.quarterly_cashflow
-
-
-# In[15]:
-
-
-ticker.earnings
-
-
-# In[16]:
-
-
-ticker.quarterly_earnings
-
-
-# ### Sustainability: Environmental, Social and Governance (ESG)
-
-# In[17]:
-
-
-ticker.sustainability
-
-
-# ### Analyst Recommendations
-
-# In[18]:
-
-
+### Analyst Recommendations
 ticker.recommendations.info()
+print(ticker.recommendations.tail(10))
 
+### Upcoming Events
+print(ticker.calendar)
 
-# In[19]:
-
-
-ticker.recommendations.tail(10)
-
-
-# ### Upcoming Events
-
-# In[20]:
-
-
-ticker.calendar
-
-
-# ### Option Expiration Dates
-
-# In[21]:
-
-
-ticker.options
-
-
-# In[22]:
-
+### Option Expiration Dates
+print(ticker.options)
 
 expiration = ticker.options[0]
-
-
-# In[23]:
-
-
 options = ticker.option_chain(expiration)
-
-
-# In[24]:
-
-
 options.calls.info()
-
-
-# In[25]:
-
-
-options.calls.head()
-
-
-# In[26]:
-
-
+print(options.calls.head())
 options.puts.info()
 
-
-# ## Data Download with proxy server
-
+## Data Download with proxy server
 # You can use a proxy server to avoid having your IP blacklisted as illustrated below (but need an actual PROXY_SERVER).
-
-# In[27]:
-
-
-PROXY_SERVER = 'PROXY_SERVER'
-
+# PROXY_SERVER = "PROXY_SERVER"
 
 # The following will only work with proper PROXY_SERVER...
-
-# In[28]:
-
-
 # msft = yf.Ticker("MSFT")
-
 # msft.history(proxy=PROXY_SERVER)
 # msft.get_actions(proxy=PROXY_SERVER)
 # msft.get_dividends(proxy=PROXY_SERVER)
@@ -219,126 +73,50 @@ PROXY_SERVER = 'PROXY_SERVER'
 # msft.get_cashflow(proxy=PROXY_SERVER)
 # msgt.option_chain(proxy=PROXY_SERVER)
 
+## Downloading multiple symbols
+tickers = yf.Tickers("msft aapl goog")
+print(tickers)
 
-# ## Downloading multiple symbols
-
-# In[29]:
-
-
-tickers = yf.Tickers('msft aapl goog')
-
-
-# In[30]:
-
-
-tickers
-
-
-# In[31]:
-
-
-pd.Series(tickers.tickers['MSFT'].info)
-
-
-# In[32]:
-
-
-tickers.tickers['AAPL'].history(period="1mo")
-
-
-# In[33]:
-
-
-tickers.history(period='1mo').stack(-1)
-
-
-# In[34]:
-
+pd.Series(tickers.tickers["MSFT"].info)
+print(tickers.tickers["AAPL"].history(period="1mo"))
+print(tickers.history(period="1mo").stack(-1))
 
 data = yf.download("SPY AAPL", start="2020-01-01", end="2020-01-05")
-
-
-# In[35]:
-
-
 data.info()
-
-
-# In[36]:
-
-
 data = yf.download(
-        tickers = "SPY AAPL MSFT", # list or string
-
-        # use "period" instead of start/end
-        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-        # (optional, default is '1mo')
-        period = "5d",
-
-        # fetch data by interval (including intraday if period < 60 days)
-        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-        # (optional, default is '1d')
-        interval = "1m",
-
-        # group by ticker (to access via data['SPY'])
-        # (optional, default is 'column')
-        group_by = 'ticker',
-
-        # adjust all OHLC automatically
-        # (optional, default is False)
-        auto_adjust = True,
-
-        # download pre/post regular market hours data
-        # (optional, default is False)
-        prepost = True,
-
-        # use threads for mass downloading? (True/False/Integer)
-        # (optional, default is True)
-        threads = True,
-
-        # proxy URL scheme use use when downloading?
-        # (optional, default is None)
-        proxy = None
-    )
-
-
-# In[37]:
-
-
+    tickers="SPY AAPL MSFT",  # list or string
+    # use "period" instead of start/end
+    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+    # (optional, default is '1mo')
+    period="5d",
+    # fetch data by interval (including intraday if period < 60 days)
+    # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+    # (optional, default is '1d')
+    interval="1m",
+    # group by ticker (to access via data['SPY'])
+    # (optional, default is 'column')
+    group_by="ticker",
+    # adjust all OHLC automatically
+    # (optional, default is False)
+    auto_adjust=True,
+    # download pre/post regular market hours data
+    # (optional, default is False)
+    prepost=True,
+    # use threads for mass downloading? (True/False/Integer)
+    # (optional, default is True)
+    threads=True,
+    # proxy URL scheme use use when downloading?
+    # (optional, default is None)
+    proxy=None,
+)
 data.info()
 
 
-# In[38]:
-
-
-from pandas_datareader import data as pdr
-
-import yfinance as yf
 yf.pdr_override()
-
 # download dataframe
-data = pdr.get_data_yahoo('SPY',
-                          start='2017-01-01',
-                          end='2019-04-30',
-                          auto_adjust=False)
+data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2019-04-30", auto_adjust=False)
+auto_adjust = True
+print(data.tail())
 
-
-# In[39]:
-
-
-# auto_adjust = True
-data.tail()
-
-
-# In[40]:
-
-
-# auto_adjust = False
-data.tail()
-
-
-# In[ ]:
-
-
-
-
+auto_adjust = False
+print(data.tail())
