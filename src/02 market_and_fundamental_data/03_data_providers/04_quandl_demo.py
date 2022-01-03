9d2f05790a93664382110693cc6591263989ffaa
mecha2k <mecha2k@naver.com>
2022-01-03 15:38:17 +0900

    market fundamental

--- src/02 market_and_fundamental_data/03_data_providers/04_quandl_demo.py
+++ src/02 market_and_fundamental_data/03_data_providers/04_quandl_demo.py
@@ -1,38 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Quandl - API Demo
-
-# Quandl uses a very straightforward API to make its free and premium data available. Currently, 50 anonymous calls are allowed, then a (free) API key is required. See [documentation](https://www.quandl.com/tools/api) for more details.
-
-# In[4]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
+# Quandl uses a very straightforward API to make its free and premium data available. Currently, 50 anonymous calls are
+# allowed, then a (free) API key is required. See [documentation](https://www.quandl.com/tools/api) for more details.
 import os
 import quandl
-
 import seaborn as sns
 import matplotlib.pyplot as plt
+from dotenv import load_dotenv
 
+load_dotenv(verbose=True)
+sns.set_style("whitegrid")
 
-# In[5]:
+api_key = os.getenv("quandl")
+oil = quandl.get("EIA/PET_RWTC_D", api_key=api_key).squeeze()
 
-
-sns.set_style('whitegrid')
-
-
-# In[6]:
-
-
-api_key = os.environ['QUANDL_API_KEY']
-oil = quandl.get('EIA/PET_RWTC_D', api_key=api_key).squeeze()
-
-
-# In[7]:
-
-
-oil.plot(lw=2, title='WTI Crude Oil Price', figsize=(12, 4))
-sns.despine()
-plt.tight_layout();
-
+oil.plot(lw=2, title="WTI Crude Oil Price", figsize=(12, 4))
+plt.tight_layout()
+plt.savefig("images/04-01.png", bboxinches="tight")
