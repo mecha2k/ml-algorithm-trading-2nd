d1defb724812cecab04fa411557be3cc3bc6b104
mecha2k <mecha2k@naver.com>
2022-01-05 15:26:17 +0900

    linear models

--- src/07 linear_models/06_evaluating_signals_using_alphalens.py
+++ src/07 linear_models/06_evaluating_signals_using_alphalens.py
@@ -1,25 +1,12 @@
 from pathlib import Path
 import pandas as pd
+
+# needs pandas version 0.20.3
 from alphalens.tears import create_summary_tear_sheet
 from alphalens.utils import get_clean_factor_and_forward_returns
 
 idx = pd.IndexSlice
-
 DATA_STORE = Path("..", "data", "assets.h5")
-
-
-with pd.HDFStore("data.h5") as store:
-    lr_predictions = store["lr/predictions"]
-    lasso_predictions = store["lasso/predictions"]
-    lasso_scores = store["lasso/scores"]
-    ridge_predictions = store["ridge/predictions"]
-    ridge_scores = store["ridge/scores"]
-
-
-# In[5]:
-
-
-# In[6]:
 
 
 def get_trade_prices(tickers, start, stop):
@@ -29,14 +16,8 @@ def get_trade_prices(tickers, start, stop):
     return prices.unstack("symbol").sort_index().shift(-1).tz_localize("UTC")
 
 
-# In[7]:
-
-
 def get_best_alpha(scores):
     return scores.groupby("alpha").ic.mean().idxmax()
-
-
-# In[8]:
 
 
 def get_factor(predictions):
@@ -49,104 +30,56 @@ def get_factor(predictions):
     )
 
 
-# ## Linear Regression
+if __name__ == "__main__":
+    with pd.HDFStore("../data/data.h5") as store:
+        lr_predictions = store["lr/predictions"]
+        lasso_predictions = store["lasso/predictions"]
+        lasso_scores = store["lasso/scores"]
+        ridge_predictions = store["ridge/predictions"]
+        ridge_scores = store["ridge/scores"]
 
-# In[9]:
+    ## Linear Regression
+    lr_factor = get_factor(lr_predictions.predicted.swaplevel())
+    lr_factor.head()
 
+    tickers = lr_factor.index.get_level_values("symbol").unique()
+    trade_prices = get_trade_prices(tickers, 2014, 2017)
+    trade_prices.info()
 
-lr_factor = get_factor(lr_predictions.predicted.swaplevel())
-lr_factor.head()
+    lr_factor_data = get_clean_factor_and_forward_returns(
+        factor=lr_factor, prices=trade_prices, quantiles=5, periods=(1, 5, 10, 21)
+    )
+    lr_factor_data.info()
 
+    create_summary_tear_sheet(lr_factor_data)
 
-# In[10]:
+    ## Ridge Regression
+    best_ridge_alpha = get_best_alpha(ridge_scores)
+    ridge_predictions = ridge_predictions[ridge_predictions.alpha == best_ridge_alpha].drop(
+        "alpha", axis=1
+    )
 
+    ridge_factor = get_factor(ridge_predictions.predicted.swaplevel())
+    print(ridge_factor.head())
 
-tickers = lr_factor.index.get_level_values("symbol").unique()
+    ridge_factor_data = get_clean_factor_and_forward_returns(
+        factor=ridge_factor, prices=trade_prices, quantiles=5, periods=(1, 5, 10, 21)
+    )
+    ridge_factor_data.info()
 
+    create_summary_tear_sheet(ridge_factor_data)
 
-# In[11]:
+    ## Lasso Regression
+    best_lasso_alpha = get_best_alpha(lasso_scores)
+    lasso_predictions = lasso_predictions[lasso_predictions.alpha == best_lasso_alpha].drop(
+        "alpha", axis=1
+    )
+    lasso_factor = get_factor(lasso_predictions.predicted.swaplevel())
+    print(lasso_factor.head())
 
+    lasso_factor_data = get_clean_factor_and_forward_returns(
+        factor=lasso_factor, prices=trade_prices, quantiles=5, periods=(1, 5, 10, 21)
+    )
+    lasso_factor_data.info()
 
-trade_prices = get_trade_prices(tickers, 2014, 2017)
-trade_prices.info()
-
-
-# In[12]:
-
-
-lr_factor_data = get_clean_factor_and_forward_returns(
-    factor=lr_factor, prices=trade_prices, quantiles=5, periods=(1, 5, 10, 21)
-)
-lr_factor_data.info()
-
-
-# In[13]:
-
-
-create_summary_tear_sheet(lr_factor_data)
-
-
-# ## Ridge Regression
-
-# In[14]:
-
-
-best_ridge_alpha = get_best_alpha(ridge_scores)
-ridge_predictions = ridge_predictions[ridge_predictions.alpha == best_ridge_alpha].drop(
-    "alpha", axis=1
-)
-
-
-# In[15]:
-
-
-ridge_factor = get_factor(ridge_predictions.predicted.swaplevel())
-ridge_factor.head()
-
-
-# In[16]:
-
-
-ridge_factor_data = get_clean_factor_and_forward_returns(
-    factor=ridge_factor, prices=trade_prices, quantiles=5, periods=(1, 5, 10, 21)
-)
-ridge_factor_data.info()
-
-
-# In[17]:
-
-
-create_summary_tear_sheet(ridge_factor_data)
-
-
-# ## Lasso Regression
-
-# In[18]:
-
-
-best_lasso_alpha = get_best_alpha(lasso_scores)
-lasso_predictions = lasso_predictions[lasso_predictions.alpha == best_lasso_alpha].drop(
-    "alpha", axis=1
-)
-
-
-# In[19]:
-
-
-lasso_factor = get_factor(lasso_predictions.predicted.swaplevel())
-lasso_factor.head()
-
-
-# In[20]:
-
-
-lasso_factor_data = get_clean_factor_and_forward_returns(
-    factor=lasso_factor, prices=trade_prices, quantiles=5, periods=(1, 5, 10, 21)
-)
-lasso_factor_data.info()
-
-
-# In[21]:
-
-
-create_summary_tear_sheet(lasso_factor_data)
+    create_summary_tear_sheet(lasso_factor_data)
--- src/07 linear_models/08_predicting_price_movements_with_logistic_regression.py
+++ src/07 linear_models/08_predicting_price_movements_with_logistic_regression.py
@@ -1,214 +1,199 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Predicting stock price moves with Logistic Regression
-
-# ## Imports & Settings
-
-# In[1]:
-
-
-import warnings
-warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
-
-from pathlib import Path
-import sys, os
-from time import time
-
 import pandas as pd
 import numpy as np
+import sys, os
 
 from scipy.stats import spearmanr
-
 from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from pathlib import Path
+from time import time
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-# In[3]:
-
-
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from utils import MultipleTimeSeriesCV
-
-
-# In[4]:
+np.random.seed(42)
+sns.set_style("whitegrid")
+plt.rcParams["figure.dpi"] = 300
+plt.rcParams["font.size"] = 16
+pd.options.display.float_format = "{:,.2f}".format
 
 
-sns.set_style('darkgrid')
-idx = pd.IndexSlice
+class MultipleTimeSeriesCV:
+    """Generates tuples of train_idx, test_idx pairs
+    Assumes the MultiIndex contains levels 'symbol' and 'date'
+    purges overlapping outcomes"""
 
+    def __init__(
+        self,
+        n_splits=3,
+        train_period_length=126,
+        test_period_length=21,
+        lookahead=None,
+        date_idx="date",
+        shuffle=False,
+    ):
+        self.n_splits = n_splits
+        self.lookahead = lookahead
+        self.test_length = test_period_length
+        self.train_length = train_period_length
+        self.shuffle = shuffle
+        self.date_idx = date_idx
 
-# In[5]:
+    def split(self, X, y=None, groups=None):
+        unique_dates = X.index.get_level_values(self.date_idx).unique()
+        days = sorted(unique_dates, reverse=True)
+        split_idx = []
+        for i in range(self.n_splits):
+            test_end_idx = i * self.test_length
+            test_start_idx = test_end_idx + self.test_length
+            train_end_idx = test_start_idx + self.lookahead - 1
+            train_start_idx = train_end_idx + self.train_length + self.lookahead - 1
+            split_idx.append([train_start_idx, train_end_idx, test_start_idx, test_end_idx])
+
+        dates = X.reset_index()[[self.date_idx]]
+        for train_start, train_end, test_start, test_end in split_idx:
+
+            train_idx = dates[
+                (dates[self.date_idx] > days[train_start])
+                & (dates[self.date_idx] <= days[train_end])
+            ].index
+            test_idx = dates[
+                (dates[self.date_idx] > days[test_start]) & (dates[self.date_idx] <= days[test_end])
+            ].index
+            if self.shuffle:
+                np.random.shuffle(list(train_idx))
+            yield train_idx.to_numpy(), test_idx.to_numpy()
+
+    def get_n_splits(self, X, y, groups=None):
+        return self.n_splits
 
 
 YEAR = 252
-
-
-# ## Load Data
-
-# In[6]:
-
-
-with pd.HDFStore('data.h5') as store:
-    data = (store['model_data']
-            .dropna()
-            .drop(['open', 'close', 'low', 'high'], axis=1))
-data = data.drop([c for c in data.columns if 'year' in c or 'lag' in c], axis=1)
-
-
-# ### Select Investment Universe
-
-# In[7]:
-
-
-data = data[data.dollar_vol_rank<100]
-
-
-# ### Create Model Data
-
-# In[8]:
-
-
-y = data.filter(like='target')
-X = data.drop(y.columns, axis=1)
-X = X.drop(['dollar_vol', 'dollar_vol_rank', 'volume', 'consumer_durables'], axis=1)
-
-
-# ## Logistic Regression
-
-# ### Define cross-validation parameters
-
-# In[9]:
-
-
-train_period_length = 63
-test_period_length = 10
-lookahead =1
-n_splits = int(3 * YEAR/test_period_length)
-
-cv = MultipleTimeSeriesCV(n_splits=n_splits,
-                          test_period_length=test_period_length,
-                          lookahead=lookahead,
-                          train_period_length=train_period_length)
-
-
-# In[10]:
-
-
-target = f'target_{lookahead}d'
-
-
-# In[11]:
-
-
-y.loc[:, 'label'] = (y[target] > 0).astype(int)
-y.label.value_counts()
-
-
-# In[12]:
-
-
-Cs = np.logspace(-5, 5, 11)
-
-
-# In[13]:
-
-
-cols = ['C', 'date', 'auc', 'ic', 'pval']
-
-
-# ### Run cross-validation
-
-# In[14]:
-
-
-get_ipython().run_cell_magic('time', '', "log_coeffs, log_scores, log_predictions = {}, [], []\nfor C in Cs:\n    print(C)\n    model = LogisticRegression(C=C,\n                               fit_intercept=True,\n                               random_state=42,\n                               n_jobs=-1)\n\n    pipe = Pipeline([\n        ('scaler', StandardScaler()),\n        ('model', model)])\n    ics = aucs = 0\n    start = time()\n    coeffs = []\n    for i, (train_idx, test_idx) in enumerate(cv.split(X), 1):\n        X_train, y_train, = X.iloc[train_idx], y.label.iloc[train_idx]\n        pipe.fit(X=X_train, y=y_train)\n        X_test, y_test = X.iloc[test_idx], y.label.iloc[test_idx]\n        actuals = y[target].iloc[test_idx]\n        if len(y_test) < 10 or len(np.unique(y_test)) < 2:\n            continue\n        y_score = pipe.predict_proba(X_test)[:, 1]\n       \n        auc = roc_auc_score(y_score=y_score, y_true=y_test)\n        actuals = y[target].iloc[test_idx]\n        ic, pval = spearmanr(y_score, actuals)\n\n        log_predictions.append(y_test.to_frame('labels').assign(\n            predicted=y_score, C=C, actuals=actuals))\n        date = y_test.index.get_level_values('date').min()\n        log_scores.append([C, date, auc, ic * 100, pval])\n        coeffs.append(pipe.named_steps['model'].coef_)\n        ics += ic\n        aucs += auc\n        if i % 10 == 0:\n            print(f'\\t{time()-start:5.1f} | {i:03} | {ics/i:>7.2%} | {aucs/i:>7.2%}')\n\n    log_coeffs[C] = np.mean(coeffs, axis=0).squeeze()")
-
-
-# ### Evaluate Results
-
-# In[15]:
-
-
-log_scores = pd.DataFrame(log_scores, columns=cols)
-log_scores.to_hdf('data.h5', 'logistic/scores')
-
-log_coeffs = pd.DataFrame(log_coeffs, index=X.columns).T
-log_coeffs.to_hdf('data.h5', 'logistic/coeffs')
-
-log_predictions = pd.concat(log_predictions)
-log_predictions.to_hdf('data.h5', 'logistic/predictions')
-
-
-# In[16]:
-
-
-log_scores = pd.read_hdf('data.h5', 'logistic/scores')
-
-
-# In[17]:
-
-
-log_scores.info()
-
-
-# In[18]:
-
-
-log_scores.groupby('C').auc.describe()
-
-
-# ### Plot Validation Scores
-
-# In[19]:
-
-
-def plot_ic_distribution(df, ax=None):
-    if ax is not None:
-        sns.distplot(df.ic, ax=ax)    
-    else:
-        ax = sns.distplot(df.ic)
-    mean, median = df.ic.mean(), df.ic.median()
-    ax.axvline(0, lw=1, ls='--', c='k')
-    ax.text(x=.05, y=.9, s=f'Mean: {mean:8.2f}\nMedian: {median:5.2f}',
-            horizontalalignment='left',
-            verticalalignment='center',
-            transform=ax.transAxes)
-    ax.set_xlabel('Information Coefficient')
-    sns.despine()
-    plt.tight_layout()
-
-
-# In[20]:
-
-
-fig, axes= plt.subplots(ncols=2, figsize=(15, 5))
-
-sns.lineplot(x='C', y='auc', data=log_scores, estimator=np.mean, label='Mean', ax=axes[0])
-by_alpha = log_scores.groupby('C').auc.agg(['mean', 'median'])
-best_auc = by_alpha['mean'].idxmax()
-by_alpha['median'].plot(logx=True, ax=axes[0], label='Median', xlim=(10e-6, 10e5))
-axes[0].axvline(best_auc, ls='--', c='k', lw=1, label='Max. Mean')
-axes[0].axvline(by_alpha['median'].idxmax(), ls='-.', c='k', lw=1, label='Max. Median')
-axes[0].legend()
-axes[0].set_ylabel('AUC')
-axes[0].set_xscale('log')
-axes[0].set_title('Area Under the Curve')
-
-plot_ic_distribution(log_scores[log_scores.C==best_auc], ax=axes[1])
-axes[1].set_title('Information Coefficient')
-
-fig.suptitle('Logistic Regression', fontsize=14)
-sns.despine()
-fig.tight_layout()
-fig.subplots_adjust(top=.9);
-
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+if __name__ == "__main__":
+    with pd.HDFStore("../data/data.h5") as store:
+        data = store["model_data"].dropna().drop(["open", "close", "low", "high"], axis=1)
+    data = data.drop([c for c in data.columns if "year" in c or "lag" in c], axis=1)
+
+    ### Select Investment Universe
+    data = data[data.dollar_vol_rank < 100]
+
+    ### Create Model Data
+    y = data.filter(like="target").copy()
+    X = data.drop(y.columns, axis=1)
+    X = X.drop(["dollar_vol", "dollar_vol_rank", "volume", "consumer_durables"], axis=1).copy()
+
+    ## Logistic Regression
+    ### Define cross-validation parameters
+    # train_period_length = 63
+    # test_period_length = 10
+    # lookahead = 1
+    # n_splits = int(3 * YEAR / test_period_length)
+    #
+    # cv = MultipleTimeSeriesCV(
+    #     n_splits=n_splits,
+    #     test_period_length=test_period_length,
+    #     lookahead=lookahead,
+    #     train_period_length=train_period_length,
+    # )
+    #
+    # target = f"target_{lookahead}d"
+    # y.loc[:, "label"] = (y[target] > 0).astype(int)
+    # print(y.label.value_counts())
+    #
+    # Cs = np.logspace(-5, 5, 11)
+    # cols = ["C", "date", "auc", "ic", "pval"]
+
+    ### Run cross-validation
+    # log_coeffs, log_scores, log_predictions = {}, [], []
+    # for C in Cs:
+    #     print(C)
+    #     model = LogisticRegression(C=C, fit_intercept=True, random_state=42, n_jobs=-1)
+    #     pipe = Pipeline([("scaler", StandardScaler()), ("model", model)])
+    #     ics = aucs = 0
+    #     start = time()
+    #     coeffs = []
+    #     for i, (train_idx, test_idx) in enumerate(cv.split(X), 1):
+    #         X_train, y_train, = (
+    #             X.iloc[train_idx],
+    #             y.label.iloc[train_idx],
+    #         )
+    #         pipe.fit(X=X_train, y=y_train)
+    #         X_test, y_test = X.iloc[test_idx], y.label.iloc[test_idx]
+    #         actuals = y[target].iloc[test_idx]
+    #         if len(y_test) < 10 or len(np.unique(y_test)) < 2:
+    #             continue
+    #         y_score = pipe.predict_proba(X_test)[:, 1]
+    #
+    #         auc = roc_auc_score(y_score=y_score, y_true=y_test)
+    #         actuals = y[target].iloc[test_idx]
+    #         ic, pval = spearmanr(y_score, actuals)
+    #
+    #         log_predictions.append(
+    #             y_test.to_frame("labels").assign(predicted=y_score, C=C, actuals=actuals)
+    #         )
+    #         date = y_test.index.get_level_values("date").min()
+    #         log_scores.append([C, date, auc, ic * 100, pval])
+    #         coeffs.append(pipe.named_steps["model"].coef_)
+    #         ics += ic
+    #         aucs += auc
+    #         if i % 10 == 0:
+    #             print(f"\t{time() - start:5.1f} | {i:03} | {ics / i:>7.2%} | {aucs / i:>7.2%}")
+    #
+    #     log_coeffs[C] = np.mean(coeffs, axis=0).squeeze()
+
+    ### Evaluate Results
+    # log_scores = pd.DataFrame(log_scores, columns=cols)
+    # log_scores.to_hdf("../data/data.h5", "logistic/scores")
+    #
+    # log_coeffs = pd.DataFrame(log_coeffs, index=X.columns).T
+    # log_coeffs.to_hdf("../data/data.h5", "logistic/coeffs")
+    #
+    # log_predictions = pd.concat(log_predictions)
+    # log_predictions.to_hdf("../data/data.h5", "logistic/predictions")
+
+    log_scores = pd.read_hdf("../data/data.h5", "logistic/scores")
+    log_scores.info()
+    print(log_scores.groupby("C").auc.describe())
+
+    ### Plot Validation Scores
+    def plot_ic_distribution(df, ax=None):
+        if ax is not None:
+            sns.histplot(data=df, x="ic", ax=ax)
+        else:
+            ax = sns.distplot(data=df, x="ic")
+        mean, median = df.ic.mean(), df.ic.median()
+        ax.axvline(0, lw=1, ls="--", c="k")
+        ax.text(
+            x=0.05,
+            y=0.9,
+            s=f"Mean: {mean:8.2f}\nMedian: {median:5.2f}",
+            horizontalalignment="left",
+            verticalalignment="center",
+            transform=ax.transAxes,
+        )
+        ax.set_xlabel("Information Coefficient")
+        plt.tight_layout()
+
+    fig, axes = plt.subplots(ncols=2, figsize=(15, 5))
+    sns.lineplot(x="C", y="auc", data=log_scores, estimator=np.mean, label="Mean", ax=axes[0])
+    by_alpha = log_scores.groupby("C").auc.agg(["mean", "median"])
+    best_auc = by_alpha["mean"].idxmax()
+    by_alpha["median"].plot(logx=True, ax=axes[0], label="Median", xlim=(10e-6, 10e5))
+    axes[0].axvline(best_auc, ls="--", c="k", lw=1, label="Max. Mean")
+    axes[0].axvline(by_alpha["median"].idxmax(), ls="-.", c="k", lw=1, label="Max. Median")
+    axes[0].legend()
+    axes[0].set_ylabel("AUC")
+    axes[0].set_xscale("log")
+    axes[0].set_title("Area Under the Curve")
+
+    plot_ic_distribution(log_scores[log_scores.C == best_auc], ax=axes[1])
+    axes[1].set_title("Information Coefficient")
+
+    fig.suptitle("Logistic Regression", fontsize=14)
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.9)
+    plt.savefig("images/08-01.png", bboxinches="tight")
