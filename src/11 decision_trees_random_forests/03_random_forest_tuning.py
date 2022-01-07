12ce4f47722136a492b28647f07332554418cd5c
mecha2k <mecha2k@naver.com>
2022-01-07 13:11:53 +0900

    decision trees, random forests

--- src/11 decision_trees_random_forests/03_random_forest_tuning.py
+++ src/11 decision_trees_random_forests/03_random_forest_tuning.py
@@ -99,10 +99,7 @@ if __name__ == "__main__":
         lookahead=lookahead,
     )
 
-    # ### Classifier
-
-    # In[13]:
-
+    ### Classifier
     rf_clf = RandomForestClassifier(
         n_estimators=100,  # default changed from 10 to 100 in version 0.22
         criterion="gini",
@@ -113,7 +110,6 @@ if __name__ == "__main__":
         max_features="auto",
         max_leaf_nodes=None,
         min_impurity_decrease=0.0,
-        min_impurity_split=None,
         bootstrap=True,
         oob_score=True,
         n_jobs=-1,
@@ -121,28 +117,17 @@ if __name__ == "__main__":
         verbose=1,
     )
 
-    # #### Cross-Validation with default settings
-
-    # In[9]:
-
+    #### Cross-Validation with default settings
     cv_score = cross_val_score(
         estimator=rf_clf, X=X, y=y_binary, scoring="roc_auc", cv=cv, n_jobs=-1, verbose=1
     )
+    print(np.mean(cv_score))
 
-    # In[10]:
-
-    np.mean(cv_score)
-
-    # ### Regression RF
-
-    # In[11]:
-
+    ### Regression RF
     def rank_correl(y, y_pred):
         return spearmanr(y, y_pred)[0]
 
     ic = make_scorer(rank_correl)
-
-    # In[12]:
 
     rf_reg = RandomForestRegressor(
         n_estimators=100,
@@ -153,7 +138,6 @@ if __name__ == "__main__":
         max_features="auto",
         max_leaf_nodes=None,
         min_impurity_decrease=0.0,
-        min_impurity_split=None,
         bootstrap=True,
         oob_score=False,
         n_jobs=-1,
@@ -162,18 +146,12 @@ if __name__ == "__main__":
         warm_start=False,
     )
 
-    # In[13]:
-
     cv_score = cross_val_score(estimator=rf_reg, X=X, y=y, scoring=ic, cv=cv, n_jobs=-1, verbose=1)
+    print(np.mean(cv_score))
 
-    # In[14]:
-
-    np.mean(cv_score)
-
-    # ## Parameter Tuning
-
-    # The key configuration parameters include the various hyperparameters for the individual decision trees introduced in the notebook [decision_trees](01_decision_trees.ipynb).
-    #
+    ## Parameter Tuning
+    # The key configuration parameters include the various hyperparameters for the individual decision trees introduced
+    # in the notebook [decision_trees](01_decision_trees.ipynb).
     # The following tables lists additional options for the two `RandomForest` classes:
 
     # | Keyword      | Default | Description                                                                                                                |
@@ -183,32 +161,41 @@ if __name__ == "__main__":
     # | oob_score    | False   | Use out-of-bag samples to estimate the R2 on unseen data                                                                   |
     # | warm_start   | False   | Reuse result of previous call to continue training and add more trees to the ensemble, otherwise, train a whole new forest |
 
-    # - The `bootstrap` parameter activates in the preceding bagging algorithm outline, which in turn enables the computation of the out-of-bag score (oob_score) that estimates the generalization accuracy using samples not included in the bootstrap sample used to train a given tree (see next section for detail).
-    # - The `n_estimators` parameter defines the number of trees to be grown as part of the forest. Larger forests perform better, but also take more time to build. It is important to monitor the cross-validation error as a function of the number of base learners to identify when the marginal reduction of the prediction error declines and the cost of additional training begins to outweigh the benefits.
-    # - The `max_features` parameter controls the size of the randomly selected feature subsets available when learning a new decision rule and split a node. A lower value reduces the correlation of the trees and, thus, the ensemble's variance, but may also increase the bias. Good starting values are `n_features` (the number of training features) for regression problems and `sqrt(n_features)` for classification problems, but will depend on the relationships among features and should be optimized using cross-validation.
+    # - The `bootstrap` parameter activates in the preceding bagging algorithm outline, which in turn enables the
+    #   computation of the out-of-bag score (oob_score) that estimates the generalization accuracy using samples not
+    #   included in the bootstrap sample used to train a given tree (see next section for detail).
+    # - The `n_estimators` parameter defines the number of trees to be grown as part of the forest. Larger forests
+    #   perform better, but also take more time to build. It is important to monitor the cross-validation error as a
+    #   function of the number of base learners to identify when the marginal reduction of the prediction error declines
+    #   and the cost of additional training begins to outweigh the benefits.
+    # - The `max_features` parameter controls the size of the randomly selected feature subsets available when learning
+    #   a new decision rule and split a node. A lower value reduces the correlation of the trees and, thus, the
+    #   ensemble's variance, but may also increase the bias. Good starting values are `n_features` (the number of
+    #   training features) for regression problems and `sqrt(n_features)` for classification problems, but will depend
+    #   on the relationships among features and should be optimized using cross-validation.
 
-    # Random forests are designed to contain deep fully-grown trees, which can be created using `max_depth=None` and `min_samples_split=2`. However, these values are not necessarily optimal, especially for high-dimensional data with many samples and, consequently, potentially very deep trees that can become very computationally-, and memory-, intensive.
-    #
-    # The `RandomForest` class provided by sklearn support parallel training and prediction by setting the n_jobs parameter to the k number of jobs to run on different cores. The -1 value uses all available cores. The overhead of interprocess communication may limit the speedup from being linear so that k jobs may take more than 1/k the time of a single job. Nonetheless, the speedup is often quite significant for large forests or deep individual trees that may take a meaningful amount of time to train when the data is large, and split evaluation becomes costly.
-    #
-    # As always, the best parameter configuration should be identified using cross-validation. The following steps illustrate the process:
+    # Random forests are designed to contain deep fully-grown trees, which can be created using `max_depth=None` and
+    # `min_samples_split=2`. However, these values are not necessarily optimal, especially for high-dimensional data
+    # with many samples and, consequently, potentially very deep trees that can become very computationally-, and
+    # memory-, intensive.
+    # The `RandomForest` class provided by sklearn support parallel training and prediction by setting the n_jobs
+    # parameter to the k number of jobs to run on different cores. The -1 value uses all available cores. The overhead
+    # of interprocess communication may limit the speedup from being linear so that k jobs may take more than 1/k the
+    # time of a single job. Nonetheless, the speedup is often quite significant for large forests or deep individual
+    # trees that may take a meaningful amount of time to train when the data is large, and split evaluation becomes
+    # costly. As always, the best parameter configuration should be identified using cross-validation. The following
+    # steps illustrate the process:
 
-    # ### Define Parameter Grid
-
-    # In[15]:
-
+    ### Define Parameter Grid
     param_grid = {
         "n_estimators": [50, 100, 250],
         "max_depth": [5, 15, None],
         "min_samples_leaf": [5, 25, 100],
     }
 
-    # ### Instantiate GridSearchCV
-
-    # We will use 10-fold custom cross-validation and populate the parameter grid with values for the key configuration settings:
-
-    # In[16]:
-
+    ### Instantiate GridSearchCV
+    # We will use 10-fold custom cross-validation and populate the parameter grid with values for the key configuration
+    # settings:
     gridsearch_clf = GridSearchCV(
         estimator=rf_clf,
         param_grid=param_grid,
@@ -220,42 +207,32 @@ if __name__ == "__main__":
         verbose=1,
     )
 
-    # ### Fit Classifier
-
-    # In[18]:
-
+    ### Fit Classifier
     gridsearch_clf.fit(X=X, y=y_binary)
 
-    # #### Persist Result
-
-    # In[19]:
-
+    #### Persist Result
     joblib.dump(gridsearch_clf, results_path / "gridsearch_clf.joblib")
 
-    # In[20]:
-
     gridsearch_clf = joblib.load(results_path / "gridsearch_clf.joblib")
+    print(gridsearch_clf.best_params_)
+    print(gridsearch_clf.best_score_)
 
-    # In[21]:
-
-    gridsearch_clf.best_params_
-
-    # In[22]:
-
-    gridsearch_clf.best_score_
-
-    # #### Feature Importance
-
-    # A random forest ensemble may contain hundreds of individual trees, but it is still possible to obtain an overall summary measure of feature importance from bagged models.
-    #
-    # For a given feature, the importance score is the total reduction in the objective function's value, which results from splits based on this feature, averaged over all trees. Since the objective function takes into account how many features are affected by a split, this measure is implicitly a weighted average so that features used near the top of a tree will get higher scores due to the larger number of observations contained in the much smaller number of available nodes. By averaging over many trees grown in a randomized fashion, the feature importance estimate loses some variance and becomes more accurate.
-    #
-    # The computation differs for classification and regression trees based on the different objectives used to learn the decision rules and is measured in terms of the mean square error for regression trees and the Gini index or entropy for classification trees.
-    #
-    # `sklearn` further normalizes the feature-importance measure so that it sums up to 1. Feature importance thus computed is also used for feature selection as an alternative to the mutual information measures we saw in Chapter 6, The Machine Learning Process (see SelectFromModel in the sklearn.feature_selection module).
+    #### Feature Importance
+    # A random forest ensemble may contain hundreds of individual trees, but it is still possible to obtain an overall
+    # summary measure of feature importance from bagged models.
+    # For a given feature, the importance score is the total reduction in the objective function's value, which results
+    # from splits based on this feature, averaged over all trees. Since the objective function takes into account how
+    # many features are affected by a split, this measure is implicitly a weighted average so that features used near
+    # the top of a tree will get higher scores due to the larger number of observations contained in the much smaller
+    # number of available nodes. By averaging over many trees grown in a randomized fashion, the feature importance
+    # estimate loses some variance and becomes more accurate.
+    # The computation differs for classification and regression trees based on the different objectives used to learn
+    # the decision rules and is measured in terms of the mean square error for regression trees and the Gini index or
+    # entropy for classification trees.
+    # `sklearn` further normalizes the feature-importance measure so that it sums up to 1. Feature importance thus
+    # computed is also used for feature selection as an alternative to the mutual information measures we saw in
+    # Chapter 6, The Machine Learning Process (see SelectFromModel in the sklearn.feature_selection module).
     # In our example, the importance values for the top-20 features are as shown here:
-
-    # In[23]:
 
     fig, ax = plt.subplots(figsize=(12, 5))
     (
@@ -265,13 +242,10 @@ if __name__ == "__main__":
         .sort_values()
         .plot.barh(ax=ax, title="RF Feature Importance")
     )
-    sns.despine()
     fig.tight_layout()
+    plt.savefig("images/03-01.png", bboxinches="tight")
 
-    # ### Fit Regressor
-
-    # In[24]:
-
+    ### Fit Regressor
     gridsearch_reg = GridSearchCV(
         estimator=rf_reg,
         param_grid=param_grid,
@@ -283,55 +257,30 @@ if __name__ == "__main__":
         verbose=1,
     )
 
-    # In[25]:
-
     gs_reg = gridsearch_reg
-
-    # In[26]:
-
     gridsearch_reg.fit(X=X, y=y)
-
-    # In[27]:
-
     joblib.dump(gridsearch_reg, results_path / "rf_reg_gridsearch.joblib")
 
-    # In[28]:
-
     gridsearch_reg = joblib.load(results_path / "rf_reg_gridsearch.joblib")
+    print(gridsearch_reg.best_params_)
+    print(f"{gridsearch_reg.best_score_*100:.2f}")
 
-    # In[29]:
-
-    gridsearch_reg.best_params_
-
-    # In[30]:
-
-    f"{gridsearch_reg.best_score_*100:.2f}"
-
-    # ### Compare Results
-
-    # #### Best Parameters
-
-    # In[31]:
-
-    pd.DataFrame(
-        {
-            "Regression": pd.Series(gridsearch_reg.best_params_),
-            "Classification": pd.Series(gridsearch_clf.best_params_),
-        }
+    ### Compare Results
+    #### Best Parameters
+    print(
+        pd.DataFrame(
+            {
+                "Regression": pd.Series(gridsearch_reg.best_params_),
+                "Classification": pd.Series(gridsearch_clf.best_params_),
+            }
+        )
     )
 
-    # #### Feature Importance
-
-    # In[32]:
-
+    #### Feature Importance
     fi_clf = gridsearch_clf.best_estimator_.feature_importances_
     fi_reg = gridsearch_reg.best_estimator_.feature_importances_
 
-    # In[33]:
-
     idx = [c.replace("_", " ").upper() for c in X.columns]
-
-    # In[34]:
 
     fig, axes = plt.subplots(figsize=(14, 4), ncols=2)
     (
@@ -348,5 +297,5 @@ if __name__ == "__main__":
         .sort_values()
         .plot.barh(ax=axes[0], title="Regression")
     )
-    sns.despine()
     fig.tight_layout()
+    plt.savefig("images/03-02.png", bboxinches="tight")
