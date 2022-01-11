999411c9adcb487605d9e4fa241692d31cd93da0
mecha2k <mecha2k@naver.com>
2022-01-11 14:54:51 +0900

    topic modeling

--- src/15 topic_modeling/03_dirichlet_distribution.py
+++ src/15 topic_modeling/03_dirichlet_distribution.py
@@ -1,69 +1,39 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Latent Dirichlet Allocation - Interactive Simulation
-
-# ## Imports
-
-# In[1]:
-
-
-import warnings
-warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
+# Latent Dirichlet Allocation - Interactive Simulation
 
 import numpy as np
 import pandas as pd
 
-# Visualization
 from ipywidgets import interact, FloatSlider
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# In[3]:
+sns.set_style("whitegrid")
+plt.rcParams["figure.dpi"] = 300
+plt.rcParams["font.size"] = 14
+pd.options.display.float_format = "{:,.2f}".format
 
 
-sns.set_style('white')
+if __name__ == "__main__":
+    ## Simulate Dirichlet Distribution
+    f = FloatSlider(
+        value=1, min=1e-2, max=10, step=1e-2, continuous_update=False, description="Alpha"
+    )
 
+    @interact(alpha=f)
+    def sample_dirichlet(alpha):
+        topics = 10
+        draws = 9
+        alphas = np.full(shape=topics, fill_value=alpha)
+        samples = np.random.dirichlet(alpha=alphas, size=draws)
 
-# ## Simulate Dirichlet Distribution
-
-# In[4]:
-
-
-f = FloatSlider(value=1,
-                min=1e-2,
-                max=10,
-                step=1e-2,
-                continuous_update=False,
-                description='Alpha')
-
-
-@interact(alpha=f)
-def sample_dirichlet(alpha):
-    topics = 10
-    draws = 9
-    alphas = np.full(shape=topics, fill_value=alpha)
-    samples = np.random.dirichlet(alpha=alphas, size=draws)
-
-    fig, axes = plt.subplots(nrows=3,
-                             ncols=3,
-                             sharex=True,
-                             sharey=True,
-                             figsize=(14, 8))
-    axes = axes.flatten()
-    plt.setp(axes, ylim=(0, 1))
-    for i, sample in enumerate(samples):
-        axes[i].bar(x=list(range(10)), height=sample,
-                    color=sns.color_palette("Set2", 10))
-    fig.suptitle('Dirichlet Allocation | 10 Topics, 9 Samples')
-    sns.despine()
-    fig.tight_layout()
-    plt.subplots_adjust(top=.95)
-
+        fig, axes = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=True, figsize=(14, 8))
+        axes = axes.flatten()
+        plt.setp(axes, ylim=(0, 1))
+        for i, sample in enumerate(samples):
+            axes[i].bar(x=list(range(10)), height=sample, color=sns.color_palette("Set2", 10))
+        fig.suptitle("Dirichlet Allocation | 10 Topics, 9 Samples")
+        sns.despine()
+        fig.tight_layout()
+        plt.subplots_adjust(top=0.95)
+        plt.savefig("images/03-01.png")
